32d75278eaba8661bcce5a6b6b9e931c6683661a
TuoKy <tuomas.kytta@gmail.com>
2015-11-17 13:25:41 +0200

    Isompi korjaus
    Plaa

---
+++ WebSite/App_Code/DataAccessLayer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for DataAccessLayer
+/// </summary>
+public class DataAccessLayer
+{
+    public DataAccessLayer()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
