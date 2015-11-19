2cd2ac7cc5b8c4493022916bb0bb619198bf75f8
TuoKy <tuomas.kytta@gmail.com>
2015-11-19 15:13:07 +0200

    T&M
    Nyt meillä on JSON. Ja kipee pää

--- WebSite/App_Code/Card.cs
+++ WebSite/App_Code/Card.cs
@@ -1,4 +1,30 @@
-﻿public class Card
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+public class Card
 {
+    public string cardId { get; set; }
     public string name { get; set; }
+    public string cardSet { get; set; }
+    public string type { get; set; }
+    public string faction { get; set; }
+    public string rarity { get; set; }
+    public int cost { get; set; }
+    public int attack { get; set; }
+    public int health { get; set; }
+    public string text { get; set; }
+    public string flavor { get; set; }
+    public string artist { get; set; }
+    public bool collectible { get; set; }
+    public string playerClass { get; set; }
+    public string howToGet { get; set; }
+    public string howToGetGold { get; set; }
+    public string img { get; set; }
+    public string imgGold { get; set; }
+    public string locale { get; set; }
+}
+public class CardDeck
+{
+    [JsonProperty("Basic")]
+    public List<Card> deck { get; set; }
 }
--- WebSite/Test.aspx.cs
+++ WebSite/Test.aspx.cs
@@ -5,22 +5,32 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Script.Serialization;
 using unirest_net.http;
+using Newtonsoft.Json;
 
 public partial class Test : System.Web.UI.Page
 {
     //Ei toimi tällä hetkellä
     protected void Page_Load(object sender, EventArgs e)
     {
-        HttpResponse<Card> response = Unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards")
-        .header("X-Mashape-Key", "notToday")
-        .asJson<Card>();
+        HttpResponse<string> response = Unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards")
+        .header("X-Mashape-Key", "????")
+        .asJson<string>();
 
-        IEnumerable<Card> sorsa = response.Body as IEnumerable<Card>;
 
-        Label1.Text = response.Body.ToString();
+        //JavaScriptSerializer ser = new JavaScriptSerializer();
+        //CardDeck plaa = ser.Deserialize<CardDeck>(response.Body);
 
-        Grid.DataSource = sorsa;
+        CardDeck plaa = new CardDeck();
+        plaa = JsonConvert.DeserializeObject<CardDeck>(response.Body);
+
+        //IEnumerable<string> sorsa = response as IEnumerable<string>;
+        Label1.Text = response.Body;
+        
+        //for ()
+
+        //Grid.DataSource = sorsa;
 
     }
 }
