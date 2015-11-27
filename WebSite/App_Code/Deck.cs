e743af82f73339c2aa5f32684e3a72ad6a70f407
mikkoleppanen <mikko.t.leppanen@hotmail.com>
2015-11-27 20:04:15 +0200

    Deck luokka

---
+++ WebSite/App_Code/Deck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Deck
+/// </summary>
+public class Deck
+{
+    public int deckId { get; set; }
+    public string name { get; set; }
+    public int userId { get; set; }
+    public List<deckHasCard> cards { get; set; }
+}
+
+public class deckHasCard
+{
+    public int cardId { get; set; }
+    public int count { get; set; }
+}
