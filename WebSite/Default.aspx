f8d2b566b24cd4f4adc0006b6665ddc5ca3114fe
TuoKy <tuomas.kytta@gmail.com>
2015-11-28 13:10:10 +0200

    ARg, vitun postback
    Kuinka luoda semmoinen nappula ettei lähetä postbackkia

--- WebSite/Default.aspx
+++ WebSite/Default.aspx
@@ -15,8 +15,13 @@
        <asp:Button ID="classCards7" runat="server" Text="Button" />
        <asp:Button ID="neutralCards" runat="server" Text="Button" />
     </div>
-    <asp:ImageButton ID="previous" runat="server" src="Images/arrowBack.png" CssClass="arrowStyle" />
-    <div id="cards">  
+    
+    
+    
+    <div id="cards"> 
+         
+        <asp:ImageButton ID="previous" runat="server" ImageUrl="Images/arrowBack.png" CssClass="arrowStyleLeft" OnClick="previous_Click" />
+
         <asp:Image ID="Image1" runat="server" CssClass="imageStyle"/>
         <asp:Image ID="Image2" runat="server" CssClass="imageStyle"/>
         <asp:Image ID="Image3" runat="server" CssClass="imageStyle"/>
@@ -25,9 +30,14 @@
         <asp:Image ID="Image5" runat="server" CssClass="imageStyle"/>
         <asp:Image ID="Image6" runat="server" CssClass="imageStyle"/>
         <asp:Image ID="Image7" runat="server" CssClass="imageStyle"/>
-        <asp:Image ID="Image8" runat="server" CssClass="imageStyle"/>         
+        <asp:Image ID="Image8" runat="server" CssClass="imageStyle"/>     
+        
+        <asp:ImageButton ID="next" runat="server" ImageUrl="Images/arrowNext.png" CssClass="arrowStyleRight" OnClick="next_Click" OnClientClick="" /> 
+            
     </div>
-    <asp:ImageButton ID="next" runat="server" src="Images/arrowNext.png" CssClass="arrowStyle" /> 
+    
+    
+    
     <div id="buttonsSide">
         <asp:Button ID="Create" runat="server" Text="Create new deck" />
         <asp:Button ID="Button2" runat="server" Text="Button" />
--- WebSite/Default.aspx.cs
+++ WebSite/Default.aspx.cs
@@ -7,45 +7,56 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
-    private DataAccessLayer layer = new DataAccessLayer();
-    private CardLogic logic = new CardLogic();
-    List<Card> cards = new List<Card>();
-    private IComparer<Card> comparer;
-
+    private CardLogic logic;   
+    
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!IsPostBack)
+        {                        
+            logic = new CardLogic();
+            setPictures(logic.index);
+        }       
+    }
+    
+    private void setPictures(int i)
+    {
+        Image1.ImageUrl = logic.cards[i].img;
+        Image2.ImageUrl = logic.cards[i + 1].img;
+        Image3.ImageUrl = logic.cards[i + 2].img;
+        Image4.ImageUrl = logic.cards[i + 3].img;
+        Image5.ImageUrl = logic.cards[i + 4].img;
+        Image6.ImageUrl = logic.cards[i + 5].img;
+        Image7.ImageUrl = logic.cards[i + 6].img;
+        Image8.ImageUrl = logic.cards[i + 7].img;
+
+        logic.index = i + 7;
+    }
+
+    protected void next_Click(object sender, ImageClickEventArgs e)
+    {
+        try
         {
-            getCards();
-            setPictures();
+            setPictures(logic.index + 1);
+        }
+        catch (Exception)
+        {
+
+            throw;
         }
         
     }
 
-    private void getCards()
+    protected void previous_Click(object sender, ImageClickEventArgs e)
     {
         try
         {
-            cards = layer.readCardsFromDB();
-            cards.Sort(new cardComparer());
+            setPictures(logic.index - 8);
         }
         catch (Exception)
         {
+
             throw;
         }
-    }
-
-    private void setPictures()
-    {
-        Image1.ImageUrl = cards[0].img;
-        Image2.ImageUrl = cards[1].img;
-        Image3.ImageUrl = cards[2].img;
-        Image4.ImageUrl = cards[3].img;
-        Image5.ImageUrl = cards[4].img;
-        Image6.ImageUrl = cards[5].img;
-        Image7.ImageUrl = cards[6].img;
-        Image8.ImageUrl = cards[7].img;
-
     }
 
 }
